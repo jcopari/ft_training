2bc8bb5d66daa2a4946af39b8a10bb9d33337abb
João Copari <jcopari@gmail.com>
2025-06-27 21:35:04 -0300

    Adicionando exercício 01

--- C00/ex01/ft_print_alphabet.c
+++ C00/ex01/ft_print_alphabet.c
@@ -1,30 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jcopari- <jcopari-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/27 11:02:04 by jcopari-          #+#    #+#             */
-/*   Updated: 2025/06/27 21:22:07 by jcopari-         ###   ########.fr       */
+/*   Created: 2025/06/27 21:34:41 by jcopari-          #+#    #+#             */
+/*   Updated: 2025/06/27 21:34:42 by jcopari-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-// Escreva uma função que exiba o caractere passado como parâmetro.
-//Funções autorizadas: Write. 
-
-void	ft_putchar(char c)
-{
-	write (1, &c, 1);
-}
-
-int	main(void)
-{
-	ft_putchar('a');
-	ft_putchar('b');
-	ft_putchar('c');
-	ft_putchar('d');
-	return (0);
-}

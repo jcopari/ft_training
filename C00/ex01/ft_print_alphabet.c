b939434108a278056d15da6a81e6be9de0ecd19b
João Copari <jcopari@gmail.com>
2025-06-29 20:59:38 -0300

    Terminando exercício 1

--- C00/ex01/ft_print_alphabet.c
+++ C00/ex01/ft_print_alphabet.c
@@ -6,7 +6,30 @@
 /*   By: jcopari- <jcopari-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 21:34:41 by jcopari-          #+#    #+#             */
-/*   Updated: 2025/06/27 21:34:42 by jcopari-         ###   ########.fr       */
+/*   Updated: 2025/06/29 20:58:45 by jcopari-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+// Escreva uma função que exiba o alfabeto em minúsculas, numa única linha, em
+// ordem crescente, começando pela letra ’a’.
+
+void	ft_print_alphabet(void)
+{
+	char	letra;
+
+	letra = 'a';
+	while (letra <= 'z')
+	{
+		write (1, &letra, 1);
+		letra++;
+	}
+	write (1, "\n", 1);
+}
+
+int	main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}
